286bb5670b604f68618b32bd5700771da9279ab7
Eugen Priymak <epriymak1@gmail.com>
2022-10-24 17:30:54 +0200

    add gameOver()

--- js/script.js
+++ js/script.js
@@ -14,13 +14,21 @@ let btnScissors = document.querySelector('.scissors');
 function getComputerChoice() {
     return computerSelection = choice[Math.floor(Math.random() * choice.length)];
 }
-function getPlayerChoice() {
-    btnPaper.addEventListener('click', () => playerSelection = "paper");
-    btnRock.addEventListener('click', () => playerSelection = "rock");
-    btnScissors.addEventListener('click', () => playerSelection = "scissors");
-
-    
-}
+// function getPlayerChoice() {
+    btnRock.addEventListener('click', () => {
+        playerSelection = 'rock';
+        playRound();
+    });
+    btnPaper.addEventListener('click', () =>  {
+        playerSelection = 'paper';
+        playRound();
+    });
+    btnScissors.addEventListener('click', () =>  {
+        playerSelection = 'scissors';
+        playRound();
+    });
+        
+// }
 
 function getRoundResult() {
     
@@ -69,40 +77,75 @@ function getRoundResult() {
 
 }
 function playRound() {
+    console.log("go");
+    getComputerChoice();
+    console.log(computerSelection);
     getRoundResult();
-    if (message == "===\nInput Error! Try again\n===") {
-    console.log(message);
-    }else {
-        if (message == "You lose") {
-            computerScore ++;
-        } else if (message == "You won") {
-            playerScore ++;
-        } else if (message == "Draw"){
-
-        }else {}
-        console.log(playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1) + " vs. " + computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1));
+    
+        if (message == "===\nInput Error! Try again\n===") {
         console.log(message);
-        console.log(`Score:\nYou ${playerScore}:${computerScore} Computer`);
-        return message;
-    }
-}
+        }else {
+            if (message == "You lose") {
+                computerScore ++;
+            } else if (message == "You won") {
+                playerScore ++;
+            } else if (message == "Draw"){
+    
+            }else {}
+            // console.log(playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1) + " vs. " + computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1));
+            console.log(message);
+            console.log(`Score:\nYou ${playerScore}:${computerScore} Computer`);
+            // return message;
+        }
+        if (computerScore == 5 || playerScore == 5) {
+        gameOver();
+        }
+        // break playRound;
 
+    }
+
+    function gameOver() {
+        if (computerScore == 5) {
+            console.log("you lose");
+        } else {
+            console.log("you win");
+        }
+        computerScore = 0;
+        playerScore = 0;
+    }
 // for (i = 1; i < 6; i++) {
-//     console.log("Round " + i);
-//     getComputerChoice();
-//     getPlayerChoice();
-//     playRound();
-//     if (message == "===\nInput Error! Try again\n===") {
-//         i-- ;}
+    // console.log("Round " + i);
+    // getComputerChoice();
+    // getPlayerChoice();
+    // playRound();
+    // if (message == "===\nInput Error! Try again\n===") {
+    //     i-- ;}
 // }
 
-if (playerScore > computerScore) {
-    console.log("You won!")
-} else if (playerScore < computerScore) {
-    console.log("You lose.")
-} else {
-    console.log("Draw.")
-}
-console.log( "Final Score: " + playerScore + " : " + computerScore);
+// if (playerScore > computerScore) {
+//     console.log("You won!")
+// } else if (playerScore < computerScore) {
+//     console.log("You lose.")
+// } else {
+//     console.log("Draw.")
+// }
+// console.log( "Final Score: " + playerScore + " : " + computerScore);
 
+// getPlayerChoice();
+console.log("h");
+    // if (message == "===\nInput Error! Try again\n===") {
+    //     console.log(message);
+    //     }else {
+    //         if (message == "You lose") {
+    //             computerScore ++;
+    //         } else if (message == "You won") {
+    //             playerScore ++;
+    //         } else if (message == "Draw"){
+    
+    //         }else {}
+    //         // console.log(playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1) + " vs. " + computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1));
+    //         console.log(message);
+    //         console.log(`Score:\nYou ${playerScore}:${computerScore} Computer`);
+// }
+// while ( computerScore <= 5 || playerScore <= 5);
 
